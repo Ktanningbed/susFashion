a0dc2fa3aae51f8c9a312c6ac77dedd46df024e1
latiu <emily.nieu@gmail.com>
2023-05-13 13:27:44 -0400

    some changes to main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import ImageUpload from './components/ImageUpload';
-import Home from './components/Home';
+import Home from './pages/Home';
 
 function App() {
   return (

1c3ce4bd74c15b1b1d1c11fc1cc74d49cbcd8a20
tanya972 <tanyalu628@gmail.com>
2023-05-13 13:29:30 -0400

    second page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import ImageUpload from './components/ImageUpload';
 import Home from './components/Home';
+import Results from './Pages/Results';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Results/>}/>
       </Routes>
     </>
   );

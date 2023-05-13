446d12e4c4717e39b1821aa80933a0f6ff50f021
Kevin <tankevin793@gmail.com>
2023-05-13 13:01:33 -0400

    react router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import ImageUpload from './components/ImageUpload';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
-      <ImageUpload/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </>
   );
 }
